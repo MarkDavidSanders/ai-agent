ba7088c603fe3e9267b053638c7b567590af4fba
Mark Sanders <140538549+MarkDavidSanders@users.noreply.github.com>
2025-12-14 14:22:55 -0500

    Docstring shenanigans

--- main.py
+++ main.py
@@ -4,13 +4,13 @@
 '''
 The program we're building is a CLI tool that:
 
-Accepts a coding task (e.g., "strings aren't splitting in my app")
-Chooses from a set of predefined functions to work on the task, for example:
-Scan the files in a directory
-Read a file's contents
-Overwrite a file's contents
-Execute the Python interpreter on a file
-Repeats step 2 until the task is complete (or it fails miserably, which is possible)
+1. Accepts a coding task (e.g., "strings aren't splitting in my app")
+2. Chooses from a set of predefined functions to work on the task, for example:
+   -Scan the files in a directory
+   -Read a file's contents
+   -Overwrite a file's contents
+   -Execute the Python interpreter on a file
+3. Repeats step 2 until the task is complete (or it fails miserably, which is possible)
 
 So that's nice!
 '''
